65bc1be5901f8c37487386996b9fb898e02d3755
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2013-07-07 04:17:36 -0500

    rename

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -4,5 +4,5 @@ git checkout master-private
 git branch -f gh-pages master-private
 git checkout gh-pages
 git update-ref -d refs/heads/master
-git commit -m "rebased"
-git checkout master-private
+git commit -m "updated"
+bower install
