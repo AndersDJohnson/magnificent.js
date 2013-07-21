3484ff89be7d07da493d4cfbcdbb0d3bc99eb29a
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2013-07-21 15:55:02 -0500

    update rebase script

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-git checkout master-private
-git branch -f gh-pages master-private
+#git checkout master-private
+git branch -f gh-pages master
 git checkout gh-pages
-git update-ref -d refs/heads/master
+#git update-ref -d refs/heads/master
 bower install
 git add -f components
 git commit -m "updated"
+git checkout master
