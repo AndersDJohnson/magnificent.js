579e081556e2be12380a220a693e3d7b3429e0c0
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-03-15 01:53:26 -0500

    resize images in example

--- example/1/index.js
+++ example/1/index.js
@@ -6,27 +6,27 @@ $host.mag();
 
 $host = $('#host-thumb');
 $host.mag({
-  content: '<img src="img/alley/1000x666.jpg" />',
-  contentThumb: '<img src="img/alley/500x333.jpg" />'
+  content: '<img src="img/alley/1000x600.jpg" />',
+  contentThumb: '<img src="img/alley/500x300.jpg" />'
 });
 
 $host = $('#host2');
 $host.mag({
-  content: '<img src="img/alley/1000x666.jpg" />',
+  content: '<img src="img/alley/1000x600.jpg" />',
   position: 'mirror',
   positionEvent: 'hold'
 });
 
 $host = $('#host3');
 $host.mag({
-  content: '<img src="img/alley/1000x666.jpg" />',
+  content: '<img src="img/alley/1000x600.jpg" />',
   position: 'joystick',
   positionEvent: 'move'
 });
 
 $host = $('#host4');
 $host.mag({
-  content: '<img src="img/alley/1000x666.jpg" />',
+  content: '<img src="img/alley/1000x600.jpg" />',
   position: 'joystick',
   positionEvent: 'hold'
 });
