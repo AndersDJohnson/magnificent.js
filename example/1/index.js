4a432137246759ce063d8124aeba6a525d12b186
Anders D. Johnson <adjohnson916@users.noreply.github.com>
2015-03-14 21:37:40 -0500

    move elements to html in example

--- example/1/index.js
+++ example/1/index.js
@@ -1,33 +1,26 @@
 
 var $host;
-var $main;
 
-$main = $('main');
-
-$host = $('<div id="host" class="host">');
-$main.append($host);
+$host = $('#host');
 $host.mag({
   content: '<img src="img/alley/1000x666.jpg" style="width: 100%; height: 100%;"/>'
 });
 
-$host = $('<div id="host2" class="host">');
-$main.append($host);
+$host = $('#host2');
 $host.mag({
   content: '<img src="img/alley/1000x666.jpg" style="width: 100%; height: 100%;"/>',
   position: 'mirror',
   positionEvent: 'hold'
 });
 
-$host = $('<div id="host3" class="host">');
-$main.append($host);
+$host = $('#host3');
 $host.mag({
   content: '<img src="img/alley/1000x666.jpg" style="width: 100%; height: 100%;"/>',
   position: 'joystick',
   positionEvent: 'move'
 });
 
-$host = $('<div id="host4" class="host">');
-$main.append($host);
+$host = $('#host4');
 $host.mag({
   content: '<img src="img/alley/1000x666.jpg" style="width: 100%; height: 100%;"/>',
   position: 'joystick',
