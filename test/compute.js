d2df3d0c2cda7f3d0a2c50bad98bff4ddf699d94
Anders D. Johnson <adjohnson916@users.noreply.github.com>
2015-04-02 18:17:17 -0500

    fix compute tests and remove project tests

--- test/compute.js
+++ test/compute.js
@@ -1,15 +1,16 @@
 var Mag = require('..');
 var assert = require('assert');
 
-var mag = new Mag();
-
 describe('compute', function () {
 
-  it('should', function () {
+  it('should 1', function () {
     var model = {
       focus: {x:0, y:0},
       zoom: 1
     };
+    var mag = new Mag({
+      model: model
+    });
     mag.compute(model);
     assert.equal(model.lens.x, 0);
     assert.equal(model.lens.y, 0);
@@ -17,11 +18,14 @@ describe('compute', function () {
     assert.equal(model.zoomed.y, 0);
   });
 
-  it('should', function () {
+  it('should 2', function () {
     var model = {
       focus: {x:0, y:0},
       zoom: 2
     };
+    var mag = new Mag({
+      model: model
+    });
     mag.compute(model);
     assert.equal(model.lens.x, 0);
     assert.equal(model.lens.y, 0);
@@ -31,63 +35,68 @@ describe('compute', function () {
     assert.equal(model.zoomed.y, 0);
   });
 
-  it('should', function () {
+  it('should 3', function () {
     var model = {
       zoom: 1
     };
+    var mag = new Mag({
+      model: model
+    });
     mag.compute(model);
     assert.equal(model.lens.x, 0);
     assert.equal(model.lens.y, 0);
   });
-  it('should', function () {
+  it('should 4', function () {
     var model = {
-      lens: {w:0.5, h:0.5},
-      focus: {x:0.5, y:0.5}
+      focus: {x:0.5, y:0.5},
+      zoom: 2
     };
+    var mag = new Mag({
+      model: model
+    });
     mag.compute(model);
     assert.equal(model.lens.x, 0.25);
     assert.equal(model.lens.y, 0.25);
     assert.equal(model.zoomed.w, 2);
     assert.equal(model.zoomed.h, 2);
-    assert.equal(model.zoomed.x, -0.25);
-    assert.equal(model.zoomed.y, -0.25);
+    assert.equal(model.zoomed.x, -0.25 * 2);
+    assert.equal(model.zoomed.y, -0.25 * 2);
   });
 
-  it('should', function () {
+  it('should 5', function () {
     var model = {
-      lens: {w:0.25, h:0.25},
-      focus: {x:0.5, y:0.5}
+      focus: {x:0, y:0},
+      zoom: 2
     };
-    mag.compute(model);
-    assert.equal(model.lens.x, 0.375);
-    assert.equal(model.lens.y, 0.375);
-  });
-
-  it('should', function () {
-    var model = {
-      lens: {w:0.5, h:0.5},
-      focus: {x:0, y:0}
-    };
+    var mag = new Mag({
+      model: model
+    });
     mag.compute(model);
     assert.equal(model.lens.x, 0);
     assert.equal(model.lens.y, 0);
   });
 
-  it('should', function () {
+  it('should 6', function () {
     var model = {
-      lens: {w:0.5, h:0.5},
-      focus: {x:1, y:1}
+      focus: {x:1, y:1},
+      zoom: 2
     };
+    var mag = new Mag({
+      model: model
+    });
     mag.compute(model);
     assert.equal(model.lens.x, 0.5);
     assert.equal(model.lens.y, 0.5);
   });
 
-  it('should', function () {
+  it('should 7', function () {
     var model = {
-      lens: {w:0.25, h:0.25},
-      focus: {x:1, y:1}
+      focus: {x:1, y:1},
+      zoom: 4
     };
+    var mag = new Mag({
+      model: model
+    });
     mag.compute(model);
     assert.equal(model.lens.x, 0.75);
     assert.equal(model.lens.y, 0.75);
