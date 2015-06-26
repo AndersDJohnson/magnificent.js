4004b7dd283dad0582419f37bf503455529029a8
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-06-26 10:24:52 -0500

    More bower verb helper work (draft)

--- helper-bower.js
+++ helper-bower.js
@@ -1,44 +1,123 @@
 
 var fs = require('fs');
+var path = require('path');
 var arrayify = require('arrayify');
 
-var parse = function () {
-  var f = fs.readFileSync('bower.json', 'utf8');
-  var j = JSON.parse(f);
+var parse = function (opts) {
+  opts = opts || {};
+  if (! opts.bowerFile) opts.bowerFile = 'bower.json';
+  var f;
+  var j;
+  try {
+    f = fs.readFileSync(opts.bowerFile, 'utf8');
+    j = JSON.parse(f);
+  }
+  catch (e) {
+    console.error('Error reading file "' + opts.bowerFile + '".', e);
+  }
+  if (opts.shim) {
+    if (!j) j = {};
+    opts.shim(opts.name, j);
+  }
   return j;
 };
 
-var mdList = function (list) {
+var times = function (str, num) {
+  var out = '';
+  for (var i = 0; i < num; ++i) {
+    out += str;
+  }
+  return out;
+};
+
+var mdList = function (list, indent) {
+  if (!indent) indent = 0;
+  var nest = times('  ', indent) + '* ';
   return list.map(function (item) {
-    return '* ' + item;
+    if (typeof item === 'string') {
+      return nest + item;
+    }
+    else {
+      return nest + item.name + (item.children ? '\n' + mdList(item.children, indent+1) : '');
+    }
   }).join('\n');
 };
 
+var getBasePathForDep = function (depName) {
+  return 'bower_components/' + depName + '/';
+};
 
-var main = function (fp, opts) {
-  j = parse();
+var getBowerFileForDep = function (depName) {
+  return getBasePathForDep(depName) + '.bower.json';
+};
+
+
+var cleanPath = function () {
+  var mp = path.join.apply(path, arguments);
+  mp = path.normalize(mp);
+  mp = mp.replace(/\\/g, '/');
+  return mp;
+};
+
+
+var mainList = function (opts) {
+  opts = opts || {};
+  if (!opts.basePath) opts.basePath = '';
+  j = parse(opts);
   var ts = [];
   if (j.main) {
     var ms = arrayify(j.main);
     ms.forEach(function (m) {
-      ts.push('`' + m + '`');
+      var mp = cleanPath(opts.basePath, m);
+      ts.push({
+        name: '`' + mp + '`'
+      });
     });
   }
-  t = mdList(ts);
+  return ts;
+};
+
+
+var main = function (opts) {
+  opts = opts || {};
+  var ts = mainList(opts);
+  t = mdList(ts, opts.indent);
   return t;
 };
 
 
-var deps = function (fp, opts) {
-  j = parse();
+var deps = function (opts) {
+  opts = opts || {};
+  j = parse(opts);
   var ts = [];
   if (j.dependencies) {
-    var ms = arrayify(j.dependencies);
-    ms.forEach(function (v, k) {
-      ts.push('`' + k + '@' + v + '`');
-    });
+    var d = j.dependencies;
+    for (var n in d) {
+      var ch;
+      try {
+        var nb = getBasePathForDep(n);
+        var nf = getBowerFileForDep(n);
+        ch = mainList({
+          basePath: nb,
+          bowerFile: nf,
+          indent: opts.indent,
+          shim: opts.shim,
+          name: n
+        });
+      } catch (e) {
+        console.error(e, e.stack);
+      }
+      var v = d[n];
+      var item = {
+        name: '`' + n + '@' + v + '`'
+      };
+      if (ch && ch.length) {
+        item.children = ch;
+      }
+      ts.push(item);
+    }
   }
-  t = mdList(ts);
+  t = mdList(ts, opts.indent);
   return t;
 };
 
