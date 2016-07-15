a003eca6beef48173f850913acbbb4a3f746f9ea
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2016-07-14 19:59:41 -0500

    style: use eslint standard
    Closes #39

--- src/js/mag-control.js
+++ src/js/mag-control.js
@@ -1,83 +1,77 @@
 (function (root, factory) {
-  var name = 'MagnificentControl';
+  var name = 'MagnificentControl'
   if (typeof define === 'function' && define.amd) {
     define(['jquery', './mag-analytics', 'jquery-bridget'], function (jQuery, MagnificentAnalytics) {
-        return (root[name] = factory(jQuery, MagnificentAnalytics));
-    });
+      return (root[name] = factory(jQuery, MagnificentAnalytics, root.screenfull))
+    })
   } else if (typeof exports === 'object') {
-    module.exports = factory(require('jquery'), require('./mag-analytics'), require('jquery-bridget'));
+    module.exports = factory(require('jquery'), require('./mag-analytics'), require('jquery-bridget'), require('screenfull'))
   } else {
-    root[name] = factory(root.jQuery, root.MagnificentAnalytics);
+    root[name] = factory(root.jQuery, root.MagnificentAnalytic, root.screenfull)
   }
-}(this, function ($, MagnificentAnalytics) {
-
+}(this, function ($, MagnificentAnalytics, screenfull) {
   var MagnificentControl = function (element, options) {
-    this.element = $( element );
-    this.options = $.extend( true, {}, this.options, options );
-    this._init();
-  };
+    this.element = $(element)
+    this.options = $.extend(true, {}, this.options, options)
+    this._init()
+  }
 
   MagnificentControl.prototype._init = function () {
+    var $el = this.element
 
-    var $el = this.element;
-    var el = $el.get(0);
+    this.$mag = $(this.options.mag)
+    this.mag = this.$mag.get(0)
+    this.magInst = this.$mag.data('mag')
 
-    this.$mag = $(this.options.mag);
-    this.mag = this.$mag.get(0);
-    this.magInst = this.$mag.data('mag');
-
-    var $mag = this.$mag;
-    var mag = this.mag;
-    var magInst = this.magInst;
+    var mag = this.mag
+    var magInst = this.magInst
 
     $el.find('[mag-ctrl-zoom-by], [data-mag-ctrl-zoom-by]')
     .on('click', function () {
-      var attr = $(this).attr('mag-ctrl-zoom-by') || $(this).attr('data-mag-ctrl-zoom-by');
-      var zoomBy = $.parseJSON(attr);
-      magInst.zoomBy(zoomBy);
-    });
+      var attr = $(this).attr('mag-ctrl-zoom-by') || $(this).attr('data-mag-ctrl-zoom-by')
+      var zoomBy = $.parseJSON(attr)
+      magInst.zoomBy(zoomBy)
+    })
 
     $el.find('[mag-ctrl-move-by-x], [mag-ctrl-move-by-y], [data-mag-ctrl-move-by-x], [data-mag-ctrl-move-by-y]')
     .on('click', function () {
-      var attr = $(this).attr('mag-ctrl-move-by-x') || $(this).attr('data-mag-ctrl-move-by-x');
-      var x = attr;
+      var attr = $(this).attr('mag-ctrl-move-by-x') || $(this).attr('data-mag-ctrl-move-by-x')
+      var x = attr
       if (x) {
-        x = $.parseJSON(x);
+        x = $.parseJSON(x)
       }
-      attr = $(this).attr('mag-ctrl-move-by-y') || $(this).attr('data-mag-ctrl-move-by-y');
-      var y = attr;
+      attr = $(this).attr('mag-ctrl-move-by-y') || $(this).attr('data-mag-ctrl-move-by-y')
+      var y = attr
       if (y) {
-        y = $.parseJSON(y);
+        y = $.parseJSON(y)
       }
       var moveBy = {
         x: x,
         y: y
-      };
-      magInst.moveBy(moveBy);
-    });
+      }
+      magInst.moveBy(moveBy)
+    })
 
     $el.find('[mag-ctrl-fullscreen], [data-mag-ctrl-fullscreen]')
     .on('click', function () {
       if (screenfull) {
         if (screenfull.enabled) {
-          screenfull.request(mag);
+          screenfull.request(mag)
         }
       }
-    });
+    })
 
     $el.find('[mag-ctrl-destroy], [data-mag-ctrl-destroy]')
     .on('click', function () {
-      magInst.destroy();
-    });
-  };
-
-
-  $.bridget('magCtrl', MagnificentControl);
-
-
-  if (MagnificentAnalytics) {
-    MagnificentAnalytics.track('mag-control.js');
+      magInst.destroy()
+    })
   }
 
-  return MagnificentControl;
-}));
+  $.bridget('magCtrl', MagnificentControl)
+
+  if (MagnificentAnalytics) {
+    MagnificentAnalytics.track('mag-control.js')
+  }
+
+  return MagnificentControl
+}))
